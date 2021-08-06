80ace31bab60b8873dc3a78dda9462610fea05b4
temurbek-4224 <78374407+temurbek-4224@users.noreply.github.com>
2021-08-06 23:16:57 +0500

    I have finished the second day of the 5 days challenge

---
+++ components/SmallCard.js
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+function SmallCard({ img, location, distance }) {
+  return (
+    <div className='flex items-center space-x-4 m-2 mt-5 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transform duration-200 ease-out'>
+      {/* Left */}
+      <div className='relative h-16 w-16'>
+        <Image
+          src={img}
+          layout='fill'
+          className='rounded-lg'
+        />
+      </div>
+      {/* Right */}
+      <div>
+        <h2>{location}</h2>
+        <h3 className='text-gray-500'>{distance}</h3>
+      </div>
+    </div>
+  )
+}
+
+export default SmallCard;
